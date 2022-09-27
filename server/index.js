9ea1ef77ba7ca878a32b91af00ba2c848894c859
TheBoy-WhoCode <jiten.pateljp2001@gmail.com>
2022-09-27 12:27:12 +0530

    signup api created

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const authRouter = require('./routes/auth')
+
+const PORT = 3000
+
+const app = express()
+
+
+app.use(express.json())
+app.use(authRouter)
+
+const DB = "mongodb+srv://thejitenpatel:test1234@cluster0.j5rx77w.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(DB).then(() => {
+    console.log("Database connected successfully!");
+}).catch((err) => {
+    console.log(err);
+})
+
+
+app.listen(PORT, "0.0.0.0", () => { 
+    console.log(`Connected at port ${PORT}` );
+})
